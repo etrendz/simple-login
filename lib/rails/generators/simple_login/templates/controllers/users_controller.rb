5332920db4996d016794cb2751a094f61992aa89
etrendz <info@etrendz.in>
2014-10-23 15:24:47 +0530

    Adding Strong Parameters to users controller

--- lib/rails/generators/simple_login/templates/controllers/users_controller.rb
+++ lib/rails/generators/simple_login/templates/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       cookies[:auth_token] = @user.auth_token
       redirect_to root_url
@@ -14,4 +14,9 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
